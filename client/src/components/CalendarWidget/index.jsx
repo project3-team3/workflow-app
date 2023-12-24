3b4b61fc041cb8b9c8637e17c5eac5d83cf00a08
kristbg <kristbg@gmail.com>
2023-12-23 21:23:15 -0500

    chore:adjusted calendar widget css

--- client/src/components/CalendarWidget/index.jsx
+++ client/src/components/CalendarWidget/index.jsx
@@ -1,9 +1,19 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
 
 const CalendarWidget = () => {
   const [date, setDate] = useState(new Date());
+
+  useEffect(() => {
+    // Find and select the elements with class react-calendar__navigation and react-calendar__month-view__weekdays
+    const navigationEl = document.querySelector(".react-calendar__navigation");
+    const daysEl = document.querySelector(
+      ".react-calendar__month-view__days"
+    );
+    // Add the class widget-prevent-drag-wf to the elements
+    navigationEl.classList.add("widget-prevent-drag-wf");
+    daysEl.classList.add("widget-prevent-drag-wf");
+  }, []);
 
   return (
     <div className="calendar-widget widget-content-wf">
